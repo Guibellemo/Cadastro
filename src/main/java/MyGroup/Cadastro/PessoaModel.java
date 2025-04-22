9612f8bc4086155b1fee6fbb978dd47f4579c9ec
Guibellemo <gbellemo@gmail.com>
2025-04-21 21:52:37 -0300

    [feat] Add PessoaModel

--- src/main/java/MyGroup/Cadastro/PessoaModel.java
+++ src/main/java/MyGroup/Cadastro/PessoaModel.java
@@ -1,4 +1,49 @@
 package MyGroup.Cadastro;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_cadastros")
 public class PessoaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    Long id;
+    private String nome;
+    private String email;
+    private int idade;
+
+    public PessoaModel() {
+    }
+
+    public PessoaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
